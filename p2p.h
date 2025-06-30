55b6c5a014b5e45ccfb666684b7346be47b5d171
Daedric <19690mtm@gmail.com>
2025-06-29 17:03:02 -0700

    Updated p2p.h + added callback functions + added a namespace group "P2P"
    This might be able to be simplified look into it

---
+++ p2p.h
@@ -0,0 +1,32 @@
+#ifndef P2P_H
+#define P2P_H
+
+#include <string>
+#include <vector>
+#include <functional>
+#include "blockchain.h"
+using namespace std;
+
+// Block          - chunk of code
+// callback       - code that gets called later
+// Block callback - you pass a block as the callback.
+
+/*  Call back functions*/
+using BlockCallback = function<void(const Block&)>;
+using TxCallback = function<void(const Transaction&)>;
+
+
+/*Namespaces - a namspace in C++ is a way to group related functions, classes, variables, under a scope.
+               it helps avoid name conflicts.*/
+
+namespace P2P 
+{
+    void startServer(int port, BlockCallback on_block, TxCallback on_tx);
+    void connectToPeer(const string& ip, int port);
+    void broadcast(const string& message);
+    void sendFile(int peer_id, const string& filepath, const string& recipient_pub_key);
+    void listPeers();
+    int getPeerCount();
+}
+
+#endif
